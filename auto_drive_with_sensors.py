7e0de389dfdceb0c3c8d14fb1191edfbea096eb7
Jerry-Desktop <unclefish7@outlook.com>
2024-10-25 10:30:49 +0800

    尝试用agent控制车辆，把每一步的坐标转换都可视化一下

--- auto_drive_with_sensors.py
+++ auto_drive_with_sensors.py
@@ -60,7 +60,7 @@ def main():
         settings.fixed_delta_seconds = 0.01  # 应小于 0.5
         world.apply_settings(settings)
 
-    total_sec = 5  # 模拟总时长（秒）
+    total_sec = 60  # 模拟总时长（秒）
     total_tick = int(total_sec / settings.fixed_delta_seconds) + 1  # 总 tick 数
 
     try:
@@ -147,8 +147,8 @@ def main():
                 # 或者通过traffic_manager为每辆车单独设置速度百分比差异
                 # traffic_manager.vehicle_percentage_speed_difference(vehicle, random.uniform(-1000, -1000))  # 车辆速度限制范围
 
-        traffic_manager.set_global_distance_to_leading_vehicle(2.5)  # 设置车辆间距
-        traffic_manager.global_percentage_speed_difference(0)      # 设置全局速度差异
+        traffic_manager.set_global_distance_to_leading_vehicle(0)  # 设置车辆间距
+        traffic_manager.global_percentage_speed_difference(-300.0)      # 设置全局速度差异
         traffic_manager.set_respawn_dormant_vehicles(True)           # 重新生成静止车辆
 
         # ----------------- UAV 设置 -----------------
@@ -168,7 +168,7 @@ def main():
         # uav3 = UAV(world, location3, uav_id=3, yaw_angle=0)
         # uavs.append(uav3)
 
-        location4 = carla.Location(x=0, y=-50, z=50)
+        location4 = carla.Location(x=50, y=0, z=50)
         uav4 = UAV(world, location4, uav_id=4, yaw_angle=0)
         uavs.append(uav4)
 
